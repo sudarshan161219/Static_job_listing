ac25a693483b89a40dc7b9d2e17e6078d87f1806
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2023-01-08 12:52:17 +0530

    08 01 2023

--- app.js
+++ app.js
@@ -1,82 +1,96 @@
-import data from './data.json' assert { type: "json" }
-const content = document.querySelector('.content')
-const ulList = document.querySelector('.ulList')
+import data from './data.json' assert { type: "json" };
+const content = document.querySelector('.content');
+const filterContent = document.querySelector('.filter-list');
+
 
 // language tags
 const language = (lang) => {
   let lan = ""
   lang.forEach((item) => {
-    lan += `<li>${item}</li>`
+    lan += `<li class="li"  >${item}</li>`
   })
   return lan;
 };
 
 // Tools tags
-const tags = ( tags) => {
+const tags = (tags) => {
   let tag = ""
   tags.forEach((item) => {
-    tag +=  `<li>${item}</li>`
+    tag += `<li class="li"  >${item}</li>`
   })
   return tag;
 }
- 
+
 data.forEach((item) => {
+  
+  const listItem = document.createElement('li')
+  listItem.className = "list"
+          listItem.innerHTML = `
+          ${item.featured ? `   <div class="border-style" ></div>` : ''} 
+      <div class="img-info-container" >
+         <div class="img-container">
+         <img class="company-logo" src=${item.logo} alt=${item.company}  />
+         </div>
+      
+        <div class="info">
+      
+           <div class="heading-container">
+              <h1 class="heading" >${item.company}</h1>
+                 <div class="featured-container">
+                     ${item.new ? `<span class="new" >new!</span>` : ''} 
+                     ${item.featured ? `<span class="featured" >featured</span>` : ''} 
+                 </div>
+          </div>
+      
+      <div class="heading-container-sec" >
+          <h2 class="position-heading" >${item.position}</h2>
+          <ul>
+          <li>${item.postedAt}</li>
+          <li> <span class="span">&#x2022</span> ${item.contract}</li>
+          <li> <span class="span">&#x2022</span> ${item.location}</li>
+          </ul>
+      </div>
+      
+        </div>
+      </div>
+      
+      <div class="tags-container">
+      <ul class="tags-list" >
+      ${language(item.languages)}
+      ${tags(item.tools)}
+      <li class="li" >${item.role}</li>
+      <li class="li" >${item.level}</li>
+      </ul>
+      </div>
+        `
 
-    const listItem = document.createElement('li')
-    listItem.className = "list"
 
-    listItem.innerHTML = `
-<div class="img-info-container" >
-   <div class="img-container">
-   <img class="company-logo" src=${item.logo} alt=${item.company}  />
-   </div>
-
-  <div class="info">
-
-     <div class="heading-container">
-        <h1 class="heading" >${item.company}</h1>
-           <div class="featured-container">
-               ${item.new ? `<span class="new" >new!</span>` : ''} 
-               ${item.featured ? `<span class="featured" >featured</span>` : ''} 
-           </div>
-    </div>
-
-<div class="heading-container-sec" >
-    <h2 class="position-heading" >${item.position}</h2>
-    <ul>
-    <li>${item.postedAt}</li>
-    <li> <span class="span">&#x2022</span> ${item.contract}</li>
-    <li> <span class="span">&#x2022</span> ${item.location}</li>
-    </ul>
-</div>
-
-  </div>
-</div>
-
-<div class="tags-container">
-<ul class="tags-list" >
-${language(item.languages)}
-${tags(item.tools)}
-<li>${item.role}</li>
-<li>${item.level}</li>
-</ul>
-</div>
-
-
-  `
-
-    content.appendChild(listItem)
-
-})
+  content.appendChild(listItem)
 
 
 
+});
 
-// Create Language Cards
-// const createLang = (langs)=>{
-//   let langCards="";
-//   langs.forEach((lang)=>{
-//       langCards += `<span class="lang filter">${lang}</span>`;
-//   });
-//   return langCards;
-// };
+const tagss = document.querySelectorAll('.li' )
+const list = document.querySelector('.list' )
+
+// let hello = data.filter((item) => {
+// //  item.languages.forEach((i) => {
+// //   return i
+// // })
+
+// return item.languages === "Python"
+
+// })
+
+// console.log(hello)
+// let ii
+// let hello = data.filter ((item) => {
+// item.languages.forEach((i) => {
+// ii = i
+// })
+// return ii ===  "Python"
+
+// })
+
+// console.log(hello)
